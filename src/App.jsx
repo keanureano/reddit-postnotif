c8ed444c1006b1945a08c613244b2538c7faa7db
Keanu Reaño <reanokeanu@gmail.com>
2023-03-28 20:55:18 +0800

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,25 @@ import React, { useState, useEffect } from "react";
 function App() {
   const [subreddit, setSubreddit] = useState("forhire");
   const [query, setQuery] = useState("hiring web");
-  const [restrictSubreddit, setRestrictSubreddit] = useState(false);
+  const [restrictSubreddit, setRestrictSubreddit] = useState(true);
   const [posts, setPosts] = useState([]);
 
-  const getPosts = (subreddit, query, restrictSubreddit) => {
-    axios
-      .get(
-        `https://www.reddit.com/r/${subreddit}/search.json?q=${query}&restrict_sr=${Number(
-          restrictSubreddit
-        )}&sort=new&limit=1000`
-      )
-      .then((result) => {
-        const newPosts = result.data.data.children.map((post) => post.data);
-        const filteredPosts = newPosts.filter((post) =>
-          /hiring/i.test(post.title)
-        );
-        console.log(filteredPosts);
-        setPosts(filteredPosts);
-      });
+  const getPosts = async (subreddit, query, restrictSubreddit) => {
+    let result;
+    if (restrictSubreddit) {
+      result = await axios.get(
+        `https://www.reddit.com/r/${subreddit}/search.json?q=${query}&restrict_sr=1&sort=new&limit=1000`
+      );
+    } else {
+      result = await axios.get(
+        `https://www.reddit.com/search.json?q=${query}&sort=new&limit=1000`
+      );
+    }
+    console.log(result);
+    const newPosts = result.data.data.children.map((post) => post.data);
+    const filteredPosts = newPosts.filter((post) => /hiring/i.test(post.title));
+    console.log(filteredPosts);
+    setPosts(filteredPosts);
   };
 
   const subredditHandler = (e) => {
@@ -46,18 +47,37 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <form>
-        <input type="text" value={subreddit} onChange={subredditHandler} />
-        <input type="text" value={query} onChange={queryHandler} />
+    <div className="px-40">
+      <form className="py-5">
         <input
+          className="mr-5 font-semibold transition duration-75 border-0 rounded-lg shadow-sm bg-slate-700 shadow-slate-500 hover:bg-slate-500"
+          type="text"
+          value={subreddit}
+          onChange={subredditHandler}
+        />
+        <input
+          className="mr-5 font-semibold transition duration-75 border-0 rounded-lg shadow-sm bg-slate-700 shadow-slate-500 hover:bg-slate-500"
+          type="text"
+          value={query}
+          onChange={queryHandler}
+        />
+        <p className="inline-block mr-1 font-semibold text-slate-300">
+          Required
+        </p>
+        <input
+          className="mr-5 rounded bg-slate-700"
           type="checkbox"
           checked={restrictSubreddit}
           onChange={restrictSubredditHandler}
         />
-        <button onClick={submitHandler}>Submit</button>
+        <button
+          className="px-4 py-1 font-semibold rounded-full shadow-sm bg-slate-400 text-slate-900 hover:bg-slate-200 shadow-slate-500"
+          onClick={submitHandler}
+        >
+          Submit
+        </button>
       </form>
-      <div>
+      <div className="grid grid-cols-3 gap-4">
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
@@ -78,15 +98,17 @@ function Post({ post }) {
     return moment(date).fromNow();
   };
   return (
-    <div>
-      <h3>{truncate(post.title, 200)}</h3>
-      <h4>r/{post.subreddit}</h4>
-      <h5>{date(post.created_utc)}</h5>
+    <a
+      className="p-4 break-words transition duration-75 rounded-lg shadow-sm bg-slate-700 hover:scale-105 hover:bg-slate-600 shadow-slate-500"
+      href={`https://www.reddit.com${post.permalink}`}
+      target="_blank"
+    >
+      <p className="mb-1 text-sm text-slate-300">
+        r/{post.subreddit} {date(post.created_utc)}
+      </p>
+      <p className="mb-5 text-xl font-semibold">{truncate(post.title, 200)}</p>
       <p>{truncate(post.selftext, 500)}</p>
-      <a href={`https://www.reddit.com${post.permalink}`} target="_blank">
-        link
-      </a>
-    </div>
+    </a>
   );
 }
 
